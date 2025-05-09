f3852fcc8afaf85de1faadc051784ee4850ee219
Bart Naudts <Bart.Naudts@codelaser.io>
2025-05-09 09:27:07 +0200

    statistics mojo

---
+++ e2immu-run-mvnplugin/src/main/java/org/e2immu/analyzer/run/mvnplugin/StatisticsMojo.java
@@ -0,0 +1,133 @@
+package org.e2immu.analyzer.run.mvnplugin;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.DependencyResolutionException;
+import org.e2immu.analyzer.shallow.analyzer.ToolChain;
+import org.e2immu.language.cst.api.element.SourceSet;
+import org.e2immu.language.cst.api.expression.ConstructorCall;
+import org.e2immu.language.cst.api.expression.MethodCall;
+import org.e2immu.language.cst.api.info.MethodInfo;
+import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.cst.api.runtime.Runtime;
+import org.e2immu.language.inspection.api.integration.JavaInspector;
+import org.e2immu.language.inspection.api.parser.ParseResult;
+import org.e2immu.language.inspection.api.resource.InputConfiguration;
+import org.e2immu.language.inspection.integration.JavaInspectorImpl;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Mojo(name = StatisticsMojo.STATISTICS_GOAL, defaultPhase = LifecyclePhase.COMPILE, threadSafe = true)
+public class StatisticsMojo extends CommonMojo {
+    public static final String STATISTICS_GOAL = "statistics";
+
+    @Parameter(property = "methodCallFrequencies", defaultValue = "${project.build.directory}/methodCallFrequencies.txt")
+    private File methodCallFrequencies;
+
+    @Parameter(property = "methodCallFrequenciesMain", defaultValue = "${project.build.directory}/methodCallFrequenciesMain.txt")
+    private File methodCallFrequenciesMain;
+
+    @Parameter(property = "methodCallFrequenciesTest", defaultValue = "${project.build.directory}/methodCallFrequenciesTest.txt")
+    private File methodCallFrequenciesTest;
+
+    @Parameter(property = "methodCallFrequenciesBySourceSet", defaultValue = "${project.build.directory}/methodCallFrequenciesBySourceSet.txt")
+    private File methodCallFrequenciesBySourceSet;
+
+    @Parameter(property = "methodCallFrequenciesMainBySourceSet", defaultValue = "${project.build.directory}/methodCallFrequenciesMainBySourceSet.txt")
+    private File methodCallFrequenciesMainBySourceSet;
+
+    @Parameter(property = "methodCallFrequenciesTestBySourceSet", defaultValue = "${project.build.directory}/methodCallFrequenciesTestBySourceSet.txt")
+    private File methodCallFrequenciesTestBySourceSet;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        try {
+            // Create output directory if it doesn't exist
+            if (methodCallFrequencies.getParentFile().mkdirs()) {
+                getLog().info("Created directories for " + methodCallFrequencies.getAbsolutePath());
+            }
+
+            InputConfiguration inputConfiguration = makeInputConfiguration();
+            JavaInspector javaInspector = new JavaInspectorImpl();
+
+            InputConfiguration withJavaModules = inputConfiguration.withE2ImmuSupportFromClasspath().withDefaultModules();
+            getLog().info("Working directory: " + withJavaModules.workingDirectory());
+            javaInspector.initialize(withJavaModules);
+
+            Runtime runtime = javaInspector.runtime();
+
+            String jdkSpec = ToolChain.extractLibraryName(javaInspector.compiledTypesManager().typesLoaded(),
+                    false);
+            String mapped = ToolChain.mapJreShortNameToAnalyzedPackageShortName(jdkSpec);
+            getLog().info("Resolved analyzed package files for " + jdkSpec + " -> " + mapped);
+
+            JavaInspector.ParseOptions parseOptions = new JavaInspectorImpl.ParseOptionsBuilder()
+                    .setFailFast(true).setDetailedSources(true).build();
+            ParseResult parseResult = javaInspector.parse(parseOptions).parseResult();
+
+            methodCallFrequencies(parseResult, methodCallFrequencies, methodCallFrequenciesBySourceSet,
+                    t -> true);
+            methodCallFrequencies(parseResult, methodCallFrequenciesMain, methodCallFrequenciesMainBySourceSet,
+                    t -> !t.compilationUnit().sourceSet().test());
+            methodCallFrequencies(parseResult, methodCallFrequenciesTest, methodCallFrequenciesTest,
+                    t -> t.compilationUnit().sourceSet().test());
+        } catch (RuntimeException | IOException | DependencyResolutionException e) {
+            throw new MojoExecutionException("Failed to write input configuration", e);
+        }
+    }
+
+    private void methodCallFrequencies(ParseResult parseResult, File freqFile, File freqFileBySourceSet,
+                                       Predicate<TypeInfo> filter) throws IOException {
+        Map<String, Integer> methodHistogram = new HashMap<>();
+        Map<String, Integer> sourceSetHistogram = new HashMap<>();
+        parseResult.primaryTypes().stream()
+                .filter(filter)
+                .flatMap(TypeInfo::recursiveSubTypeStream)
+                .flatMap(TypeInfo::constructorAndMethodStream)
+                .forEach(mi -> {
+                    mi.methodBody().visit(e -> {
+                        MethodInfo methodInfo = null;
+                        if (e instanceof MethodCall mc &&
+                            !parseResult.primaryTypes().contains(mc.methodInfo().typeInfo().primaryType())) {
+                            methodInfo = mc.methodInfo();
+                        } else if (e instanceof ConstructorCall cc && cc.constructor() != null
+                                   && !parseResult.primaryTypes().contains(cc.constructor().typeInfo().primaryType())) {
+                            methodInfo = cc.constructor();
+                        }
+                        if (methodInfo != null) {
+                            methodHistogram.merge(methodInfo.fullyQualifiedName(), 1, Integer::sum);
+                            SourceSet sourceSet = methodInfo.typeInfo().compilationUnit().sourceSet();
+                            if (sourceSet != null) {
+                                sourceSetHistogram.merge(sourceSet.name(), 1, Integer::sum);
+                            }
+                        }
+                        return true;
+                    });
+                });
+        writeHistogram(methodHistogram, freqFile);
+        writeHistogram(sourceSetHistogram, freqFileBySourceSet);
+    }
+
+    private void writeHistogram(Map<String, Integer> methodHistogram, File freqFile) throws IOException {
+        try (OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(freqFile), StandardCharsets.UTF_8)) {
+            methodHistogram.entrySet().stream()
+                    .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+                    .forEach(e -> {
+                        try {
+                            osw.append(String.valueOf(e.getValue())).append(" ").append(e.getKey()).append("\n");
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    });
+        }
+    }
+}
